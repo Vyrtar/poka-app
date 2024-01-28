236e3bc9798c8cdedec68f5bdaa46cc5bb8d3fec
sam <samuel.woerner.f@outlook.de>
2024-01-28 23:35:18 +0100

    added iterator to Replayer

--- src/components/hands-replayer/Replayer.js
+++ src/components/hands-replayer/Replayer.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect} from 'react';
 import { Button, Form, Col } from 'react-bootstrap';
 
 const Replayer = () => {
@@ -6,34 +6,77 @@ const Replayer = () => {
         actions: [],
     });
 
+    const [positions, setPositions] = useState(['SB', 'BB', 'UTG', 'UTG+1', 'MP','HJ','LJ', 'CO', 'BTN']);
+    const [currentPosIndex, setCurrentPosIndex] = useState(0);
     const [currentPosition, setCurrentPosition] = useState('SB');
     const [currentPotSize, setCurrentPotSize] = useState(0);
     const [currentAction, setCurrentAction] = useState('posts');
-    const [currentBet, setCurrentBet] = useState(1);
-    const [inputData, setInputData] = useState(2);
+    const [currentBet, setCurrentBet] = useState(0);
+    const [raiseAmount, setRaiseAmount] = useState(2);
 
     const handleAction = (type) => {
-        console.log(type);
+        
+        setCurrentAction(type)
+
+        switch (type) {
+            case 'folds':
+                
+                break;
+            case 'checks':
+                
+                break;
+            case 'calls':
+                
+                break;
+            case 'raises':
+                
+            break;
+            default:
+                console.log('idk man');
+                break;
+        }
+
+        addActionToData(type);
+        setCurrentPosition(setNextPosition);
     }
 
-    const handleInputChange = (event) => {
-        setInputData(event.target.value)
+    const handleRaiseInput = (event) => {
+        setRaiseAmount(event.target.value)
         console.log(event.target.value);
     };
 
-    const addAction = (type, bet, pot) => {
+    const addActionToData = (type) => {
         const newAction = {
             actionID: pokerHand.actions.length + 1,
             position: currentPosition,
             actionType: type,
-            actionSize: bet,
+            actionSize: currentBet,
         };
 
         setPokerHand((prevPokerHand) => ({
             ...prevPokerHand,
             actions: [...prevPokerHand.actions, newAction],
         }));
+
+        
+        console.log(pokerHand)
     };
+
+    const setNextPosition = () => {
+        const nextElement = positions[currentPosIndex];
+        setCurrentPosIndex((currentPosIndex + 1) % positions.length);
+        return nextElement;
+    };
+
+    useEffect(() => {
+        console.log('Good morning, replayer');
+        setCurrentBet(1)
+        handleAction('posts')
+        setCurrentBet(2)
+        handleAction('posts')
+        
+        
+    }, []);
 
     return (
         <>
@@ -42,18 +85,20 @@ const Replayer = () => {
 
 
                 </div>
+                <div>Current Player: {currentPosition}, Current Bet: {currentBet}, Current Pot: {currentPotSize}</div>
                 <div style={styles.inputContainer} >
-                    <Button variant='secondary' className='m-1' onClick={() => handleAction("fold")}>Fold</Button>
-                    <Button variant='secondary' className='m-1' onClick={() => handleAction("check")}>Check</Button>
-                    <Button variant='secondary' className='m-1' onClick={() => handleAction("call")}>Call</Button>
-                    <Button variant='secondary' className='m-1' onClick={() => handleAction("raise")}>Raise</Button>
+                    <Button variant='secondary' className='m-1' onClick={() => handleAction("folds")}>Fold</Button>
+                    <Button variant='secondary' className='m-1' onClick={() => handleAction("checks")}>Check</Button>
+                    <Button variant='secondary' className='m-1' onClick={() => handleAction("calls")}>Call</Button>
+                    <Button variant='secondary' className='m-1' onClick={() => handleAction("raises")}>Raise</Button>
                     <Col xs="auto">
-                        <Form.Control style={styles.input} placeholder="Raise amount" />
+                        <Form.Control style={styles.input} placeholder="Raise amount" onChange={handleRaiseInput} />
                     </Col>
                 </div>
                 <div style={styles.output}>
                     <div>
-                        <h2>Actions:</h2>
+                        <p>Actions:</p>
+                       
                         {pokerHand.actions.map((action) => (
                             <div style={{ border: 1 }} key={action.actionID}>
                                 {`${action.position} ${action.actionType} ${action.actionSize}`}
--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,6 +1,4 @@
-import Replayer from "../components/hands-replayer/Replayer"
-
 const Upload = () => {
-    return <div>Hi, this feature isnt done yet, come back later</div>
+    return <div>Hi, this isnt done yet, come back later</div>
 } 
 export default Upload
